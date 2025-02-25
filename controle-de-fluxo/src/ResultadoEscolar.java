5745ed903301bcd01466f927b4af4049528d0940
Rodrigo <chocoliris@msn.com>
2025-02-25 11:52:28 -0300

    testes fluxo

--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,18 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 10;
-        if (nota == 7)
+        int nota = 7;
+        String resultado = nota>=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+        
+        /*String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println("O resultado é " + resultado);*/
+
+        /*if (nota == 7)
         System.out.println("Aprovado");
         else if(nota >= 5 && nota < 7)
         System.out.println("Prova de recuperação");
         else
-        System.out.println("Reprovado");
+        System.out.println("Reprovado");*/
 
         }
 }
